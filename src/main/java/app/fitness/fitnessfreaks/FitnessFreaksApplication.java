55e0f50cda9a81046a840394aa8dfd1ec1049f3f
rpych <dragon0710@wp.pl>
2019-05-09 21:08:30 +0200

    add project init files

---
+++ src/main/java/app/fitness/fitnessfreaks/FitnessFreaksApplication.java
@@ -0,0 +1,13 @@
+package app.fitness.fitnessfreaks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessFreaksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessFreaksApplication.class, args);
+	}
+
+}
