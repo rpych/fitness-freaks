bd9c699a036580f060fa0cdbdfad7d3f110e3ce4
rpych <dragon0710@wp.pl>
2019-05-13 23:21:08 +0200

    add basic configuration files for spring boot

--- src/main/java/app/fitness/fitnessfreaks/FitnessFreaksApplication.java
+++ src/main/java/app/fitness/fitnessfreaks/FitnessFreaksApplication.java
@@ -1,9 +1,19 @@
 package app.fitness.fitnessfreaks;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan(basePackages={"app.fitness"})
+@EnableJpaRepositories(basePackages="app.fitness.repositories")
+@EnableTransactionManagement
+@EntityScan(basePackages="app.fitness.implementations")
 public class FitnessFreaksApplication {
 
 	public static void main(String[] args) {
